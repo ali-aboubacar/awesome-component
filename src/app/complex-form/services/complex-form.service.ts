042fe4ebe133c6a15afc6a9d835030f9045eb296
System Administrator <root@MacBook-Pro-de-Ali.local>
2023-05-28 23:48:41 +0200

    first animation commit

---
+++ src/app/complex-form/services/complex-form.service.ts
@@ -0,0 +1,20 @@
+import { Observable, catchError, delay, mapTo, of } from "rxjs";
+import { ComplexFormValue } from "../models/complex-form-value.model";
+import { environment } from "src/environments/environments";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+@Injectable()
+export class ComplexFormService {
+  constructor(private http: HttpClient) {}
+
+  saveUserInfo(formValue: ComplexFormValue): Observable<boolean> {
+    return this.http.post(`${environment.apiUrl}/users`, formValue).pipe(
+      mapTo(true),
+      delay(1000),
+      catchError(() => of(false).pipe(
+        delay(1000)
+      ))
+    );
+  }
+}
